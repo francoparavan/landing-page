06ea2c7bd4c924de81a22788d67b95cdf961a356
Franco Paraván <francoparavan@gmail.com>
2024-06-16 23:40:52 -0300

    Update card.jsx

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Card = (props) => {
+export const Card = ({url, title, description, buttonURL, buttonLabel}) => {
     return (
         <div className="card h-100 text-center border-0">
-            <img src={props.url} className="card-img-top" alt="..." />
+            <img src={url} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.description}</p>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
             </div>
             <div class="card-footer">
-                <a href={props.buttonURL} className="btn btn-danger">{props.buttonLabel}</a>
+                <a href={buttonURL} className="btn btn-danger">{buttonLabel}</a>
             </div>
         </div>
     )
